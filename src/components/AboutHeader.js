90c9b59ce8212102466fa5944f2102500128ce9a
Mia Bacchu <jewelranabacchu@gmail.com>
2022-06-28 01:21:09 +0600

    skill added

--- src/components/AboutHeader.js
+++ src/components/AboutHeader.js
@@ -12,10 +12,10 @@ const AboutHeader = () => {
             <Text variant='h3' title='Get to Know' style={{color:'white'}}/>
         <Text variant='h1' title="About Me" style={{color:'#F5F5DC',marginBottom:'2rem'}}/>
         <Grid container justifyContent='center'>
-            <Grid xs={11} md={5.5} sx={{marginX:'1rem', marginBottom:'5rem'}}>
+            <Grid xs={11} md={5} sx={{marginX:'1rem', marginBottom:'5rem'}}>
         <Image style={{borderRadius:'1rem'}} src='https://i.ibb.co/4WDDtMw/DSC-0027-1.jpg'></Image>
         </Grid>
-        <Grid xs={11} md={5.5} sx={{marginX:'1rem', marginBottom:'5rem', wordBreak:'break-words',py:'1rem',px:'1rem'}}>
+        <Grid xs={11} md={5} sx={{marginX:'1rem', marginBottom:'5rem', wordBreak:'break-words',py:'1rem',px:'1rem'}}>
           <Text sx={{color:'#F5F5DC'}} variant='h2' title="Hi, I'm Mia Bacchu"/>
           <Text sx={{color:'#F5F5DC',marginBottom:'1rem'}} variant='h3' title="A Professional Front-End Developer"/>
           <Text sx={{color:'#e6e6e6',marginBottom:'1rem'}} variant='h3' title="I am very passionate and always ready to learn and improve myself. Love to solve complicated problems and do something different. I want to take web development to a professional level in my life."/>
@@ -24,13 +24,13 @@ const AboutHeader = () => {
         <Button type='submit' variant="contained"><Text variant='h3' title='Contact Me'></Text></Button>
         </Link>
         </Grid>
-        <Grid xs={11} md={5.5} sx={{marginX:'1rem',backgroundColor:'#8A2BE2',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
+        <Grid xs={11} md={5} sx={{marginX:'1rem',backgroundColor:'#8A2BE2',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
             <SiKnowledgebase style={{pb:'.5rem',color:'whitesmoke',fontSize:'4rem'}}/>
           <Text sx={{pb:'1rem',color:'whitesmoke'}} variant='h1' title='Experience'></Text>
           <Text sx={{pb:'1rem',color:'whitesmoke'}} variant='h2' title='1+ Years'></Text>
           <Text sx={{pb:'1rem',color:'whitesmoke'}} variant='h2' title='Working'></Text>
         </Grid>
-        <Grid xs={11} md={5.5} sx={{marginX:'1rem',backgroundColor:'#8A2BE2',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
+        <Grid xs={11} md={5} sx={{marginX:'1rem',backgroundColor:'#8A2BE2',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
             <GoProject style={{pb:'.5rem',color:'whitesmoke',fontSize:'4rem'}}/>
           <Text sx={{pb:'1rem',color:'whitesmoke'}} variant='h1' title='Projects'></Text>
           <Text sx={{pb:'1rem',color:'whitesmoke'}} variant='h2' title='30+'></Text>
--- src/components/AboutSkill.js
+++ src/components/AboutSkill.js
@@ -1,29 +1,78 @@
 import { Box, Grid } from '@mui/material';
 import React from 'react';
 import Text from '../customization/Text';
+import {TiTick} from "react-icons/ti";
 
 const AboutSkill = () => {
     return (
         <Box sx={{flexGrow: 1, backgroundColor:'#333333',paddingY:'3rem'}}>
             <Text variant='h2' title='What skill I have' style={{color:'white',marginBottom:'2rem'}}/>
         <Grid container justifyContent='center'>
-        <Grid xs={11} md={5.5} sx={{marginX:'1rem',backgroundColor:'gray',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
-          <Text variant='h2' title='Front-end development'/>
-          <Box sx={{display:'flex',flexDirection:'row'}}>``
+        <Grid xs={11} md={5} sx={{marginX:'1rem',backgroundColor:'gray',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
+          <Text sx={{paddingBottom:'2rem',color:'blue'}} variant='h2' title='Front-End Development'/>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around'}}>
           <Box>
-            <Text variant='h2' title='HTML5'></Text>
-            <Text variant='h2' title='Javascript'></Text>
-            <Text variant='h2' title='Tailwind'></Text>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'center',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='HTML5'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Javascript'></Text></Box>
+          <Text variant='h3' title='Intermediate'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Tailwind'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
           </Box>
           <Box>
-            <Text variant='h2' title='CSS3'></Text>
-            <Text variant='h2' title='Bootstrap'></Text>
-            <Text variant='h2' title='React'></Text>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='CSS3'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='React'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Bootstrap'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
           </Box>
           </Box>
         </Grid>
-        <Grid xs={11} md={5.5} sx={{marginX:'1rem',backgroundColor:'gray',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
-        <Text variant='h2' title='Back-end development'/>
+        <Grid xs={11} md={5} sx={{marginX:'1rem',backgroundColor:'gray',marginBottom:'5rem',wordBreak:'break-all',py:'2rem',px:'1rem',borderRadius:'1rem'}}>
+        <Text sx={{paddingBottom:'2rem',color:'blue'}} variant='h2' title='Back-End Development'/>
+        <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around'}}>
+          <Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Node JS'></Text></Box>
+          <Text variant='h3' title='Intermediate'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='MongoDb'></Text></Box>
+          <Text variant='h3' title='InterMediate'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Heroku'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
+          </Box>
+          <Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Express JS'></Text></Box>
+          <Text variant='h3' title='Intermediate'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Firebase'></Text></Box>
+          <Text variant='h3' title='Experienced'></Text>
+          </Box>
+          <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around',color:'#F5F5DC'}}><Text variant='h2' title={<TiTick/>}></Text><Text variant='h2' title='Php'></Text></Box>
+          <Text variant='h3' title='Basic'></Text>
+          </Box>
+          </Box>
+          </Box>
         </Grid>
         </Grid>
         </Box>
