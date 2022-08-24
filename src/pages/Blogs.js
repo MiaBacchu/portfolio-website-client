fd94b5c113468cf305696568b3aa274448ae689f
Mia Bacchu <jewelranabacchu@gmail.com>
2022-08-24 23:25:16 +0600

    game added

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,14 +1,25 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import React from 'react';
 import { Fade } from 'react-reveal';
 import Text from '../customization/Text';
 
 const Blogs = () => {
+    const playGame = ()=>{
+        window.open('https://glittering-mandazi-c390cc.netlify.app', '_blank');
+    }
     return (
-        <Box sx={{height:'40rem',backgroundColor:'#36448A'}}>
-            <Fade bottom>
-            <Text sx={{color:'#D1D7E2'}} style={{paddingTop:'10rem', color:'white'}} variant="h1" title="Coming Soon"></Text>
+        <Box sx={{height:'40rem', paddingTop:'10rem',backgroundColor:'#36448A'}}>
+
+            <Fade top>
+            <Button onClick={playGame} >
+            <Text style={{backgroundColor:'#1da1f2',color:'white',padding:'.5rem'}} variant="h1" title="Check My Game">
+            </Text>
+            </Button>
             </Fade>
+
+            <Fade bottom>
+            <Text sx={{color:'#D1D7E2'}} style={{paddingTop:'10rem', paddingBottom:'10rem', color:'white'}} variant="h1" title="Blogs are Coming Soon"></Text>
+            </Fade>   
         </Box>
     );
 };
